803328c0f6fd31c17aea457f0a6dcfa3d27689b8
root <root@ip-172-31-22-86.us-west-1.compute.internal>
2018-03-26 22:00:16 +0000

    New docker image

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "key_name" {
+  description = "Name of AWS key pair"
+  default = "nodejs_cluster"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "AWS instance type"
+}
+
+variable "asg_min" {
+  description = "Min numbers of servers in ASG"
+  default     = "1"
+}
+
+variable "asg_max" {
+  description = "Max numbers of servers in ASG"
+  default     = "2"
+}
+
+variable "asg_desired" {
+  description = "Desired numbers of servers in ASG"
+  default     = "1"
+}
+
+variable "admin_cidr_ingress" {
+  description = "CIDR to allow tcp/22 ingress to EC2 instance" 
+  default = "0.0.0.0/0"
+}
+
+variable "imageid" {
+  description = "Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type "
+  default = "ami-1853ac65"
+}
